8aebe3751a48300e12bb4b75f524b31bdf0902e9
Shruti Kumari <97784397+shruti682@users.noreply.github.com>
2024-04-12 22:33:23 +0530

    Update index.js

--- OOPS in JavaScript/prototype/index.js
+++ OOPS in JavaScript/prototype/index.js
@@ -37,3 +37,9 @@ obj1.deposit(200);
 console.log(obj1);
 obj1.withdraw(300);
 console.log(obj1);
+
+
+// Prototype
+// Every object in JavaScript has a built-in property, which is called its prototype. 
+// The prototype is itself an object, so the prototype will have its own prototype, making what's called a prototype chain. 
+// The chain ends when we reach a prototype that has null for its own prototype.
